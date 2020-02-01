b20ea3cd0b6942247d82f97767fa6599fefa44ce
Armando Meza Leyva <armezaleyva@gmail.com>
2020-02-01 12:21:30 -0700

    Sistema de guardado 1.0

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    public float speed = 10f;
+    public bool completedFirstPuzzle = false;
+    public bool completedSecondPuzzle = false;
+    public bool completedThirdPuzzle = false;
+
+    public void SaveGameState()
+    {
+        SaveSystem.SaveGameState(this);
+    }
+
+    public void LoadGameState()
+    {
+        GameStateData data = SaveSystem.LoadGameState();
+
+        Vector3 position;
+        position.x = data.position[0];
+        position.y = data.position[1];
+        position.z = data.position[2];
+        transform.position = position;
+
+        completedFirstPuzzle = data.completedFirstPuzzle;
+        completedSecondPuzzle = data.completedSecondPuzzle;
+        completedThirdPuzzle = data.completedThirdPuzzle;
+    }
+
+    private void Update() 
+    {
+        //Debug.Log("UPDATE");
+        // Placeholder
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("SAVING");
+            SaveGameState();
+        }
+        else if (Input.GetKeyDown(KeyCode.L))
+        {
+            Debug.Log("LOADING");
+            LoadGameState();
+        }
+    }
+}
+
---
+++ Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveSystem
+{
+    public static void SaveGameState(GameState gameState) 
+    {
+        string path = Application.persistentDataPath + "/player.fun";
+
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream stream = new FileStream(path, FileMode.Create);
+        GameStateData data = new GameStateData(gameState);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static GameStateData LoadGameState()
+    {
+        string path = Application.persistentDataPath + "/player.fun";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+            GameStateData data = formatter.Deserialize(stream) as GameStateData;
+
+            stream.Close();
+
+            return data;
+        } else
+        {
+            Debug.LogError("Save file not found in" + path);
+            return null;
+        }
+    }
+}
