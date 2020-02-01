9fe3f8e2e414131cbe301720702f485c5fe14319
Fernanda <acquaentertainment@gmail.com>
2020-02-01 15:07:05 -0700

    guardar

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -5,10 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public float speed = 10f;
+    public bool completedFirstPuzzle = false;
+    public bool completedSecondPuzzle = false;
+    public bool completedThirdPuzzle = false;
+    public string scenename;
+
     public void NewGame()
     {
         Debug.Log("New game starts");
         SceneManager.LoadScene("SampleScene");
+    }
+    public void LoadGame(){
+        GameStateData data = SaveSystem.LoadGameState();
+        SceneManager.LoadScene(data.scenename);
+
+        completedFirstPuzzle = data.completedFirstPuzzle;
+        completedSecondPuzzle = data.completedSecondPuzzle;
+        completedThirdPuzzle = data.completedThirdPuzzle;
     }
     public void Exit(){
         Debug.Log("Close window");
