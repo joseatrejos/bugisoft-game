0e92ad63404614dc8ecc7159092319e9cbd8b06d
Armando Meza Leyva <armezaleyva@gmail.com>
2020-02-01 12:21:30 -0700

    Sistema de guardado 1.0

---
+++ Assets/Scripts/GameStateData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameStateData
+{
+    public float[] position;
+    public bool completedFirstPuzzle;
+    public bool completedSecondPuzzle;
+    public bool completedThirdPuzzle;
+
+    public GameStateData(GameState gameState) 
+    {
+        position = new float[3];
+        position[0] = gameState.transform.position.x;
+        position[1] = gameState.transform.position.y;
+        position[2] = gameState.transform.position.z;
+
+        completedFirstPuzzle = gameState.completedFirstPuzzle;
+        completedSecondPuzzle = gameState.completedSecondPuzzle;
+        completedThirdPuzzle = gameState.completedThirdPuzzle;
+    }
+}
