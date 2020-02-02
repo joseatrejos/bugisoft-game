13dafcfae1df78b46add31a43c6206b97065822b
Ana Lucia Blanco <analuciablanco@outlook.com>
2020-02-01 20:59:35 -0700

    Timer/Countdown added

---
+++ Assets/Scripts/Timer/Countdown.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Countdown : MonoBehaviour
+{
+    float currentTime = 0f;
+    [SerializeField] float startingTime = 10;
+
+    [SerializeField] TextElement countdownTxt;
+
+    private void Start() {
+        currentTime = startingTime;
+    }
+
+    private void Update() {
+        currentTime -= 1 * Time.deltaTime;
+        countdownTxt.text = currentTime.ToString();
+    }
+
+}
