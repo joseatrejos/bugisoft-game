fe36c6b90301a09357b23bb52259ae043abe37f4
Fernanda <acquaentertainment@gmail.com>
2020-02-02 14:04:20 -0700

    menuglitch

---
+++ Assets/Scripts/Menu/Menunoplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menunoplay : MonoBehaviour
+{
+    public AudioClip impact;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    public void heheh(){
+        SceneManager.LoadScene("Level01");
+    }
+    public void aresureaudio(){
+        audioSource.PlayOneShot(impact, 0.7F);
+    }
+}
