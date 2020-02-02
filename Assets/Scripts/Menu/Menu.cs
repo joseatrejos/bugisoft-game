d6480cd1096c416b8cfc8c1b2484b867b678f8de
Fernanda <acquaentertainment@gmail.com>
2020-02-02 13:48:12 -0700

    load game

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
@@ -29,12 +31,18 @@ public class Menu : MonoBehaviour
     }
     
     public void LoadGame(){
-        GameStateData data = SaveSystem.LoadGameState();
-        SceneManager.LoadScene(data.scenename);
-        completedFirstPuzzle = data.completedFirstPuzzle;
-        completedSecondPuzzle = data.completedSecondPuzzle;
-        completedThirdPuzzle = data.completedThirdPuzzle;
-        
+        string path = Application.persistentDataPath + "/player.fun";
+        if (File.Exists(path))
+        {
+            GameStateData data = SaveSystem.LoadGameState();
+
+            SceneManager.LoadScene(data.scenename);
+            completedFirstPuzzle = data.completedFirstPuzzle;
+            completedSecondPuzzle = data.completedSecondPuzzle;
+            completedThirdPuzzle = data.completedThirdPuzzle;
+        } else {
+            Debug.Log("Save file not found in" + path);
+        }
     }
     public void Exit(){
         Debug.Log("Cierra pantalla");
